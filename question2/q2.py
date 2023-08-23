56ee1608ab8173a3ac73c3353f2018c58ede4ab5
utprg <108605758+utprg@users.noreply.github.com>
2023-08-23 13:52:39 +0900

    Add files via upload

---
+++ question2/q2.py
@@ -0,0 +1,31 @@
+import gettime
+results={}
+timepoints={}
+n=int(input())
+try:
+    while True:
+        timepoint,address,result=map(str,input().split(","))
+        if address not in results.keys():
+           results[address]=[result]
+           timepoints[address]=[timepoint]
+        else:
+            results[address].append(result)
+            timepoints[address].append(timepoint)
+except EOFError:
+        pass
+for e in results.keys():
+    if "-" in results[e]:
+        i=0
+        start=0
+        err=False
+        while i<len(results[e]):
+            if err==False and results[e][i:i+n]==["-"]*n:
+                start=i
+                err=True
+            elif err==True and results[e][i]!="-":
+                err=False
+                print(e,gettime.gettime(timepoints[e][i],results[e][i])-gettime.gettime(timepoints[e][start],0),"since",gettime.gettime(timepoints[e][start],0))
+            if i==len(results[e])-1 and err:
+                print(e,"under error since",gettime.gettime(timepoints[e][start],0))
+                break
+            i+=1
